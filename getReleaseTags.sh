5174fa5f8115acc402a5b82b44f506c1e1d9a47e
davidk <kdavid@gmail.com>
2017-07-18 22:49:06 -0700

    Fix how we find the latest tag

--- getReleaseTags.sh
+++ getReleaseTags.sh
@@ -7,10 +7,10 @@ set -eu
 LATEST_RELEASE="https://api.github.com/repos/prusa3d/slic3r/releases/latest"
 
 # Get the latest tagged version
-LATEST_VERSION=$(curl -SsL ${LATEST_RELEASE} | jq -r '.tag_name | select(test("version_.+"))' | tr -d 'version_')
+LATEST_VERSION=$(curl -SsL ${LATEST_RELEASE} | jq -r '.tag_name | select(test("version_[0-9].+[0-9]$"))' | tr -d 'version_')
 
 # Get the latest tag (by tag date, not commit) in our repository
-LATEST_GIT_TAG=$(git for-each-ref refs/tags --sort=taggerdate --format='%(refname:short)' --count=1)
+LATEST_GIT_TAG=$(git for-each-ref refs/tags --sort=-creatordate --format='%(refname:short)' --count=1)
 
 if [[ "${LATEST_GIT_TAG}" != "${LATEST_VERSION}" ]]; then
   echo "Update needed. Latest tag ver: ${LATEST_GIT_TAG} != upstream ver: ${LATEST_VERSION} .."
