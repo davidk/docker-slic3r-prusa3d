96aff5a639e66f172a7560ca79137a26d77355ff
davidk <kdavid@gmail.com>
2017-08-20 01:29:18 +0000

    Run from script WD

--- checkForLatestSlic3r.sh
+++ checkForLatestSlic3r.sh
@@ -9,12 +9,14 @@ LATEST_RELEASE="https://api.github.com/repos/prusa3d/slic3r/releases/latest"
 # Get the latest tagged version
 LATEST_VERSION=$(curl -SsL ${LATEST_RELEASE} | jq -r '.tag_name | select(test("version_[0-9].+[0-9]$"))' | tr -d 'version_')
 
+# Run from the git repository
+cd "$(dirname "$0")";
+
 # Get the latest tag (by tag date, not commit) in our repository
 LATEST_GIT_TAG=$(git for-each-ref refs/tags --sort=-creatordate --format='%(refname:short)' --count=1)
-
 if [[ "${LATEST_GIT_TAG}" != "${LATEST_VERSION}" ]]; then
   echo "Update needed. Latest tag ver: ${LATEST_GIT_TAG} != upstream ver: ${LATEST_VERSION} .."
-  git tag ${LATEST_VERSION}
+  git tag "${LATEST_VERSION}"
   git push --tags
 else
   echo "Latest tag ver: ${LATEST_GIT_TAG} == upstream ver: ${LATEST_VERSION} -- no update"
