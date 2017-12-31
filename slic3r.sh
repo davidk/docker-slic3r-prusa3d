17e0025b717c5046383930b952c9df7d740fc8e7
davidk <kdavid@gmail.com>
2017-12-31 02:03:53 -0800

    Remove :z from volume mount
    closes #1

--- slic3r.sh
+++ slic3r.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Place this in your $PATH and chmod +x so you can run it like a regular program
 # Change paths, etc as appropriate
-docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/Slic3r/3d:z -v slic3rSettings:/home/slic3r -e DISPLAY=$DISPLAY --rm keyglitch/docker-slic3r-prusa3d
+
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $PWD:/Slic3r/3d \
+-v slic3rSettings:/home/slic3r \
+-e DISPLAY=$DISPLAY \
+--rm keyglitch/docker-slic3r-prusa3d
 
 # For debugging -- had something blow up inside the persisted settings volume and don't want to nuke it?
-# docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/Slic3r/3d:z -v slic3rSettings:/home/slic3r -e DISPLAY=$DISPLAY --entrypoint=/bin/bash -it keyglitch/docker-slic3r-prusa3d 
+#docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
+#-v $PWD:/Slic3r/3d:z \
+#-v slic3rSettings:/home/slic3r \
+#-e DISPLAY=$DISPLAY \
+#--entrypoint=/bin/bash \
+#-it keyglitch/docker-slic3r-prusa3d 
