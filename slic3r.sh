97d0917f4ee056f4ede06c393e85f53e97e8c61a
davidk <kdavid@gmail.com>
2019-01-27 00:49:16 -0800

    shellcheck: slic3r.sh

--- slic3r.sh
+++ slic3r.sh
@@ -3,9 +3,9 @@
 # Change paths, etc as appropriate
 
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
--v $PWD:/Slic3r/3d \
+-v "$PWD":/Slic3r/3d \
 -v slic3rSettings:/home/slic3r \
--e DISPLAY=$DISPLAY \
+-e DISPLAY="$DISPLAY" \
 --rm keyglitch/docker-slic3r-prusa3d
 
 # For debugging -- had something blow up inside the persisted settings volume and don't want to nuke it?
