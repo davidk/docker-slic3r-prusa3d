5261ee26e0e921f1cc51e0eb26ece90fd9f4c908
david <davidk@users.noreply.github.com>
2019-03-05 13:01:45 -0800

    Update gcode

--- gcode/entrypoint.sh
+++ gcode/entrypoint.sh
@@ -1,14 +1,23 @@
 #!/bin/sh -l
 
 set -eu
+
 #WORKDIR="/Slic3r/3d"
 WORKDIR="/github/workspace"
 SLICE_CFG=$1; shift
+
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_USER"
 
 for stl in "$@"; do
 	echo "Generating STL for $stl ..."
 	if sh -c "/Slic3r/slic3r-dist/slic3r --load ${WORKDIR}/${SLICE_CFG} --save ${WORKDIR}/${SLICE_CFG} --slice ${WORKDIR}/${stl}"; then
 		echo "Successfully generated STL for ${stl} ..."
+
+		git add . --force
+		git commit -m "slic3r: Generated gcode for ${stl} using ${SLICE_CFG}"
+		git push --set-upstream origin master
+		
 	else
 		exit_code=$?
 		echo "Failure generating STL, exited with ${exit_code}"
