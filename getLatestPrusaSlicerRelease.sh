4d155b763a87f4ee5a714ee3241be6c26c6afaa6
davidk <kdavid@gmail.com>
2020-10-22 23:11:15 -0700

    getLatestPrusaSlicerRelease: Prefer non-GTK3 releases for container build

--- getLatestPrusaSlicerRelease.sh
+++ getLatestPrusaSlicerRelease.sh
@@ -45,12 +45,12 @@ if [[ $# -gt 1 ]]; then
 fi
 
 if [[ "$1" == "url" ]]; then
-  
-  echo "${releaseInfo}" | jq -r '.assets[] | .browser_download_url | select(test("PrusaSlicer-.+(-\\w)?.linux-x64-.+.tar.bz2"))'
+
+  echo "${releaseInfo}" | jq -r '.assets[] | .browser_download_url | select(test("PrusaSlicer-.+(-\\w)?.linux-x64-(?!GTK3).+.tar.bz2"))'
 
 elif [[ "$1" == "name" ]]; then
 
-  echo "${releaseInfo}" | jq -r '.assets[] | .name | select(test("PrusaSlicer-.+(-\\w)?.linux-x64-.+.tar.bz2"))'
+  echo "${releaseInfo}" | jq -r '.assets[] | .name | select(test("PrusaSlicer-.+(-\\w)?.linux-x64-(?!GTK3).+.tar.bz2"))'
 
 elif [[ "$1" == "url_ver" ]]; then
 
